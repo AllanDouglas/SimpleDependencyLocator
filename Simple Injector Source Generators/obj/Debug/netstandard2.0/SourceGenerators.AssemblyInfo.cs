1d93d82b2ac49a990cc87aeda3a7d4c3f3dfa7ef
Allan Douglas <allandouglas@Mac-mini-de-Allan.local>
2026-03-01 00:31:32 -0300

    add logs to generate file

--- Simple Injector Source Generators/obj/Debug/netstandard2.0/SourceGenerators.AssemblyInfo.cs
+++ Simple Injector Source Generators/obj/Debug/netstandard2.0/SourceGenerators.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SourceGenerators")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+def64806c150e44d41604f983d898e39e39b23f3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a957a561afb35004929f49960931b2beffb17ba2")]
 [assembly: System.Reflection.AssemblyProductAttribute("SourceGenerators")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SourceGenerators")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
