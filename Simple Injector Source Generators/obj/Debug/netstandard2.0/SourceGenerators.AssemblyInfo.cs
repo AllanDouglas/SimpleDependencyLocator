20fb12cee5b05deaa94e86779cc129d98d2301cc
Allan Douglas <allandouglas@Mac-mini-de-Allan.local>
2026-03-08 11:07:29 -0300

    logs

--- Simple Injector Source Generators/obj/Debug/netstandard2.0/SourceGenerators.AssemblyInfo.cs
+++ Simple Injector Source Generators/obj/Debug/netstandard2.0/SourceGenerators.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SourceGenerators")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a68ec05ee894654ecdf75a5df7df39a517fcb7e1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c8956d471b5639475283af3cf853ef4c9823bf5")]
 [assembly: System.Reflection.AssemblyProductAttribute("SourceGenerators")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SourceGenerators")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
