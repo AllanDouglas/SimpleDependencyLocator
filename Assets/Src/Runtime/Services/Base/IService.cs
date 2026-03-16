ae0a337d9ff4588db2f3ccdc60a78953a889bbc9
Allan Douglas <allandouglas@Mac-mini-de-Allan.local>
2026-03-16 12:34:20 -0300

    add Iinitializable

--- Assets/Src/Runtime/Services/Base/IService.cs
+++ Assets/Src/Runtime/Services/Base/IService.cs
@@ -1,4 +1,21 @@
+using System;
+using UnityEngine;
+
 namespace Injector
 {
     public interface IService { }
+
+    public interface IInitializable
+    {
+        void Initialize(Action<InitializationState> action = null);
+        Awaitable<InitializationState> InitializeAsync();
+        InitializationState GetInitializationState();
+
+        public enum InitializationState
+        {
+            UNINITIALIZED,
+            INITIALIZING,
+            INITIALIZED
+        }
+    }
 }
