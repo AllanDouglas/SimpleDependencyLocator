b160ee3b29642286ead170e507e10fbf6b1b123d
Allan Douglas <allandouglas@Mac-mini-de-Allan.local>
2026-04-23 18:57:54 -0300

    try fix namespace on generated fields

--- Simple Injector Source Generators/obj/Debug/netstandard2.0/SourceGenerators.AssemblyInfo.cs
+++ Simple Injector Source Generators/obj/Debug/netstandard2.0/SourceGenerators.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SourceGenerators")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad417be4c1712b276aaa86646fa68680e04caf1e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fcaeb612372156b3c37d7c66933ec9f1ec824304")]
 [assembly: System.Reflection.AssemblyProductAttribute("SourceGenerators")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SourceGenerators")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
